c93d4ea2f4ebb3bb56d99d45df0d991dccc62918
quangthanh010290 <thanh@autonomous.nyc>
2017-08-02 12:56:48 +0700

    add rpi binary

--- deskApp/upDownTest/build/RPI/GNU-Linux/main.o.d
+++ deskApp/upDownTest/build/RPI/GNU-Linux/main.o.d
@@ -1,1 +1,33 @@
-build/RPI/GNU-Linux/main.o: main.cpp
+build/RPI/GNU-Linux/main.o: main.cpp /usr/local/include/zmq.h \
+ UpDownTest.hpp \
+ /home/thanh/ws/Linux/myLib/Library/inc/ZMQ/ZmqRequester.hpp \
+ /home/thanh/ws/Linux/myLib/Library/inc/ZMQ/iZmq.hpp \
+ /home/thanh/ws/Linux/myLib/Library/inc/COMMUNICATION/iCommunication.hpp \
+ /home/thanh/ws/Linux/myLib/Library/inc/ZMQ/ZmqReplier.hpp \
+ /home/thanh/ws/Linux/myLib/Library/inc/TIMER/Timer.hpp \
+ /home/thanh/ws/Linux/myLib/Library/inc/SmartDesk/Desk.hpp \
+ /home/thanh/ws/Linux/myLib/Library/inc/SmartDesk/LinearDevice.hpp \
+ /home/thanh/ws/Linux/myLib/Library/inc/ZMQ/ZmqReplier.hpp \
+ /home/thanh/ws/Linux/myLib/Library/inc/Device/AutonomousCommFrame.h
+
+/usr/local/include/zmq.h:
+
+UpDownTest.hpp:
+
+/home/thanh/ws/Linux/myLib/Library/inc/ZMQ/ZmqRequester.hpp:
+
+/home/thanh/ws/Linux/myLib/Library/inc/ZMQ/iZmq.hpp:
+
+/home/thanh/ws/Linux/myLib/Library/inc/COMMUNICATION/iCommunication.hpp:
+
+/home/thanh/ws/Linux/myLib/Library/inc/ZMQ/ZmqReplier.hpp:
+
+/home/thanh/ws/Linux/myLib/Library/inc/TIMER/Timer.hpp:
+
+/home/thanh/ws/Linux/myLib/Library/inc/SmartDesk/Desk.hpp:
+
+/home/thanh/ws/Linux/myLib/Library/inc/SmartDesk/LinearDevice.hpp:
+
+/home/thanh/ws/Linux/myLib/Library/inc/ZMQ/ZmqReplier.hpp:
+
+/home/thanh/ws/Linux/myLib/Library/inc/Device/AutonomousCommFrame.h:
